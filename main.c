8e405e9e259a6125521980c39b878bf184882284
uchihaitachim <duynang2k1@gmail.com>
2025-03-09 11:43:59 -0700

    them toan.h

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<math.h>
+#include"toan.h"
 
 void main() {
 	int a, b, c, d;
@@ -8,9 +9,9 @@ void main() {
 	b = tru(9, 5);
 	c = nhan(1, 7);
 	d = chia(9, 3);
-	printf("%d", a);
-	printf("%d", b);
-	printf("%d", c);
-	printf("%d", d);
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
+	printf("%d\n", d);
 }
  
