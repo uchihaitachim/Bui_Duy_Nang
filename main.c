f4e38585214405f1af27501e7c18fa9829c61c13
uchihaitachim <duynang2k1@gmail.com>
2025-03-16 10:18:46 -0700

    dem ky tu

--- main.c
+++ main.c
@@ -3,21 +3,4 @@
 #include<math.h>
 #include"toan.h"
 
-void main() {
-	int myArr[5];
-	int size = 5;	
-	nhap(myArr, size);
-	int x;
-	x=tongmang(myArr, size);
-	float tble;
-	printf("\n-----------\n");
-	tbsole(myArr, size);
-	printf("\n----------- \n");
-	printf("sum:%d", x);
-	printf("\n-----------\n");
-	inmax(myArr, size);
-	printf("\n-----------\n");
-	thayso0(myArr, size);
-	printf("\n-----------\n");
-	in_so_nguyen_to(myArr, size);
-}
+
--- toan.c
+++ toan.c
@@ -1,77 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<math.h>
-//nhap kich thuowc mang va cac phan tu trong mang tu ban phim
-void nhap(int arr[], int kt) {
-	for (int i = 0;i < kt;i++) {
-		printf("nhap vao gia tri arr[%d]:", i);
-		scanf("%d", &arr[i]);
-	}
-}
-//tinh tong cac pt trong mang
-int tongmang(int arr[], int kt) {
-	int i;
-	int sum = 0;
-	for (i = 0;i < kt;i++) {
-		sum += arr[i];
-	}
-	return sum;
-}
-//tinh trung binh cac so le
-void tbsole(int arr[], int kt) {
-	float tong = 0;
-	float j = 0;
-	float a;
-	for (int i = 0;i < kt;i++) {
-		if (arr[i] % 2 == 1) {
-			tong = tong + arr[i];
-			j++;
-		}
-	}
-	a = (float)(tong / j);
-	printf("trung binh cac so le: %.4f", a);
-}
-//in ra man so max va vi tri cua no
-void inmax(int arr[], int kt) {
-	int vitri = 0;
-	int somax = arr[0];
-	for (int i = 1;i < kt;i++) {
-		if (arr[i] > somax) {
-			somax = arr[i];
-			vitri = i;
-		}
-	}
-	printf("so lon nhat la %d vi tri thu %d", somax, vitri);
-}
-//thay so am bang so 0 va in ra mang sau khi thay
-void thayso0(int arr[], int kt) {
-	for (int i = 0;i < kt;i++) {
-		if (arr[i] < 0) {
-			arr[i] = 0;
-		}
-	}
-	printf("mang sau thay so am bang so 0:\n");
-	for (int i = 0;i < kt;i++) {
-		printf("%d ", arr[i]);
-	}
-}
-void in_so_nguyen_to(int arr[], int kt) {
-	printf("mang co cac so nguyen to la:\n");
-	for (int i = 0;i < kt;i++) {
-		int a = 1;
-		if (arr[i] < 2) {
-			a = 0;
-		}
-		else {
-			for (int j = 2;j <=sqrt(arr[i]);j++) {
-				if (arr[i] % j == 0) {
-					a = 0;
-					break;
-				}
-			}
-		}
-		if (a == 1) { 
-			printf("%d ", arr[i]);
-		}
-	}
+
+// dem ky tu trong mang
+void main() {
+	char arr[] = "helobaby";
+	int so_ky_tu;
+	so_ky_tu = sizeof(arr) / sizeof(arr[0]);
+	printf("%d", so_ky_tu);
 }
