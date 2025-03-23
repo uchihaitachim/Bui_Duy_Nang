028baefd518f1f41c556e089d01ef0b37948839b
uchihaitachim <duynang2k1@gmail.com>
2025-03-23 09:37:12 -0700

    con tro mang

--- main.c
+++ main.c
@@ -2,5 +2,22 @@
 #include<stdio.h>
 #include<math.h>
 #include"toan.h"
+//dem ky tu trong chuoi dung con tro
+void main() {
+	char arr[] = "heloc baby come on baby";
+	int x = dem_ky_tu(arr);
+	int y = dem_space(arr);
+	printf("so ky tu cua chuoi la: %d\n", x);
+	printf("so ky tu space cua chuoi la: %d\n", y);
+	//in_hoa(arr);
+	dem_so_tu(arr);
+	in_hoa_ki_tu(arr);
+	printf("\n");
+	tim_dc_c(arr);
+	
+}
+
+
+
 
 
