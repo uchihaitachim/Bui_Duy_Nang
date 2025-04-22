70356bdbd994c7fd28118e97d10f475b3667651e
uchihaitachim <duynang2k1@gmail.com>
2025-04-22 10:22:26 -0700

    test_union

--- main.c
+++ main.c
@@ -2,30 +2,25 @@
 #include <stdio.h>
 #include<stdint.h>
 #include<malloc.h>
-typedef struct {
-	char bit_0 : 1;
-	char bit_1 : 1;
-	char bit_2 : 1;
-	char bit_3 : 1;
-	char bit_4 : 1;
-	char bit_5 : 1;
-	char bit_6 : 1;
-	char bit_7 : 1;
-}byte_t;
-typedef struct {
-	int x;
-	char y;
-}test_struct_t;
 typedef union {
-	unsigned short x;
-	unsigned char y[2];
+	struct {
+		char bit_0 : 1;
+		char bit_1 : 1;
+		char bit_2 : 1;
+		char bit_3 : 1;
+		char bit_4 : 1;
+		char bit_5 : 1;
+		char bit_6 : 1;
+		char bit_7 : 1;
+	}bit;
+	unsigned char toan_bo_gia_tri;
+
 }test_union_t;
 void main() {
-	unsigned short val = 0x1234;
-	unsigned char low;
-	unsigned char high;
-	test_union_t n;
-	n.x = val;
-	low = n.y[0];
-	high = n.y[1];
-} 
+	test_union_t x = { 0 };
+	x.bit.bit_0 = 1;
+	x.bit.bit_2 = 1;
+	int temp=x.toan_bo_gia_tri;
+	// 0x00000101 = 5= temp
+	
+}	
